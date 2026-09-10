2138a99cdfa46b8cf8941684678151d6586e62ef
drrezapiri <dr.reza.piri@gmail.com>
2026-09-10 11:51:11 +0200

    Clean up unused attributes in __init__.py
    Removed unused attributes related to series and navigation.

--- src/piriview_core/__init__.py
+++ src/piriview_core/__init__.py
@@ -3,7 +3,3 @@
 Open-source research software for medical image visualization,
 segmentation, and quantitative analysis.
 """
-
-self.series = {}
-self.active_series = []
-self.navigation = NavigationService()
